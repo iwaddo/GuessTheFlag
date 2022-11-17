df603b8acd65e97088db5db8d6ba7901c7cb7dc8
Ian Waddington <iwaddo@icloud.com>
2022-11-17 22:18:08 +0000

    Before starting the project after Day 20 ready for Day 21

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,15 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Button("Show Alert") {
+                    showingAlert = true
+                }
+                .alert("Important message", isPresented: $showingAlert) {
+                    Button("Delete", role: .destructive) { }
+                        Button("Cancel", role: .cancel) { }
+                } message: {
+                    Text("Please read this.")
+                }
+        
+        
+//        VStack {
+//            Button("Delete Selection", role: .destructive, action: executeDelete)
+//
+//            VStack {
+//                Button("Button 1") { }
+//                    .buttonStyle(.bordered)
+//                Button("Button 2", role: .destructive) { }
+//                    .buttonStyle(.bordered)
+//                Button("Button 3") { }
+//                    .buttonStyle(.borderedProminent)
+//                Button("Button 4", role: .destructive) { }
+//                    .buttonStyle(.borderedProminent)
+//            }
+//        }
+
+        
     }
+        
+//        func executeDelete() {
+//            print("Now Deleting!!")
+//        }
+        
+//        VStack {
+//            LinearGradient(gradient: Gradient(stops: [
+//                Gradient.Stop(color: .white, location: 0.45),
+//                .init(color: .black, location: 0.55)
+//            ]), startPoint: .top, endPoint: .bottom)
+//
+//            RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
+//
+//            AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
+//        }
+        //
+//
+//        ZStack {
+//            VStack {
+//                Color.red
+//                Color.blue
+//            }
+//
+//
+//            Color(red: 1, green: 0.8, blue:0)
+//                .ignoresSafeArea()
+//            Color.blue
+//            Color.secondary
+//                .frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
+//
+//
+//            Text("Your Content")
+//                .foregroundStyle (.secondary)
+//                .padding(50)
+//                .background(.ultraThinMaterial)
+//        }
+        
+//    }
+    
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
